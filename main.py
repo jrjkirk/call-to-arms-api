578506e2749c23cd44da4722c6ba1e91239f3266
jrjkirk <jrjkirk1@gmail.com>
2026-05-11 01:31:16 +0100

    Allow Vercel origin in CORS

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://call-to-arms-web.vercel.app"
         # We'll add the Vercel URL here later, once we deploy the frontend.
     ],
     allow_credentials=True,
